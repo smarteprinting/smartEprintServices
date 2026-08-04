d825717c3446e38938d57a541d19eaff674653ee
unknown <vandanalodhi2005@gmail.com>
2026-08-05 01:01:43 +0530

    test deleted

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,7 +34,11 @@ export default function RootLayout({ children }) {
           src="https://cdn-cookieyes.com/client_data/c10dbbbd8867014a9030d7802875f74c/script.js"
         />
 
-        <Script src="//code.jivosite.com/widget/d7JjftxKYx" async></Script>
+        <Script
+          id="jivo-chat"
+          src="https://code.jivosite.com/widget/d7JjftxKYx"
+          strategy="afterInteractive"
+        />
         <SiteLayout>{children}</SiteLayout>
       </body>
     </html>
