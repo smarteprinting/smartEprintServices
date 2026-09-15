65c71d7b7742e8f796d162dd0c61ff45ce117b41
unknown <vandanalodhi2005@gmail.com>
2026-09-15 05:30:28 +0530

    jivo

--- app/layout.jsx
+++ app/layout.jsx
@@ -36,11 +36,11 @@ export default function RootLayout({ children }) {
           />
         )}
 
-        {/* <Script
+        <Script
           id="jivo-chat"
           src="https://code.jivosite.com/widget/d7JjftxKYx"
           strategy="afterInteractive"
-        /> */}
+        />
         <Script id="bing-uet" strategy="afterInteractive">
           {`(function(w, d, t, u, o) {w[u] = w[u] || [], o.ts = (new Date).getTime();var n = d.createElement(t);n.src = "https://bat.bing.net/bat.js?ti=" + o.ti + ("uetq" != u ? "&q=" + u : ""),n.async = 1, n.onload = n.onreadystatechange = function() {var s = this.readyState;s && "loaded" !== s && "complete" !== s ||(o.q = w[u], w[u] = new UET(o), w[u].push("pageLoad"),n.onload = n.onreadystatechange = null)};var i = d.getElementsByTagName(t)[0];i.parentNode.insertBefore(n, i);})(window, document, "script", "uetq", {ti: "187265607",enableAutoSpaTracking: true});`}
         </Script>
